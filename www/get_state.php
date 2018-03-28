c2c0db562879983da3a3649c26fc2d8d5a6bb128
antoine.scherrer <antoine.scherrer@lecole-ldlc.com>
2018-03-28 14:22:03 +0200

    update get_state

--- www/get_state.php
+++ www/get_state.php
@@ -1,6 +1,12 @@
 <?php
+    $file = "/tmp/coop_serial.txt";
+    $c = "?";
+    if (file_exists($file)){
+        $c = file_get_contents("/tmp/coop_serial.txt");
+    } else {
+        $c = "? (file not found)";
+    }
 
- $c = file_get_contents("/tmp/coop_serial.txt");
  echo($c);
 
 ?>
