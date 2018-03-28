75278828c298f0e192bf130ef8be8d4ab0cb0832
antoine.scherrer <antoine.scherrer@lecole-ldlc.com>
2018-03-28 15:09:04 +0200

    add php script for commands

---
+++ www/emulate_button.php
@@ -0,0 +1,7 @@
+<?php
+    if (isset($_GET["button"]){
+        $button = $_GET["button"];
+        echo exec('/usr/bin/python3 /home/pi/chicken_coop/scripts/coop_button.py ' . $button . '');
+    }
+    
+?>
