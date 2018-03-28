1d2d345c202a31eb4115f9f2eda333af8bca2fd3
antoine.scherrer <antoine.scherrer@lecole-ldlc.com>
2018-03-28 15:19:39 +0200

    minor update

--- scripts/coop_button.py
+++ scripts/coop_button.py
@@ -23,8 +23,13 @@ if __name__ == "__main__":
     but = sys.argv[1]
     GPIO.setmode(GPIO.BOARD)
     if but == "OPEN":
+        print("Force open door")
         change_gpio(OPEN_DOOR)
     elif but == "CLOSE":
+        print("Force close door")
         change_gpio(CLOSE_DOOR)
     elif but == "LED":
+        print("Force led toggle")
         change_gpio(TOGGLE_LED)
+    else:
+        print("unknown command")
