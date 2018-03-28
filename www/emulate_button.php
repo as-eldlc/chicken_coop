2dabe6c0cabf1a830b53f522ad4e814a6cb53d10
antoine.scherrer <antoine.scherrer@lecole-ldlc.com>
2018-03-28 16:08:24 +0200

    minor update

--- www/emulate_button.php
+++ www/emulate_button.php
@@ -1,5 +1,5 @@
 <?php
-    if (isset($_GET["button"]){
+    if (isset($_GET["button"])){
         $button = $_GET["button"];
         echo exec('/usr/bin/python3 /home/pi/chicken_coop/scripts/coop_button.py ' . $button . '');
     }
