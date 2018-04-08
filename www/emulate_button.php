38d81103af42eed10669c0d5a6a8b69ec9d98dea
Antoine Scherrer <picakoch@gmail.com>
2018-04-08 20:10:24 +0200

    minor mods

--- www/emulate_button.php
+++ www/emulate_button.php
@@ -4,7 +4,7 @@
         $pass = $_GET["pass"];
         if ($pass == "grotte!"){
             echo exec('/usr/bin/python3 /home/pi/chicken_coop/scripts/coop_button.py ' . $button . '');
-            echo "OK";
+            echo " OK";
         } else {
             echo "bad password !";
         }
