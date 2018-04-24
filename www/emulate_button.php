e2ddbc329f113a22f5424def2e27a298f7b1b96a
antoine.scherrer <antoine.scherrer@lecole-ldlc.com>
2018-04-24 11:21:21 +0200

    use sha1 instead of clear password to prevent people from playing with our coop !

--- www/emulate_button.php
+++ www/emulate_button.php
@@ -2,7 +2,7 @@
     if (isset($_GET["button"]) && isset($_GET["pass"])){
         $button = $_GET["button"];
         $pass = $_GET["pass"];
-        if ($pass == "grotte!"){
+        if (sha1($pass) == "245262042730d3520eedcc33de4870d99ce3be1e"){
             echo exec('/usr/bin/python3 /home/pi/chicken_coop/scripts/coop_button.py ' . $button . '');
             echo " OK";
         } else {
